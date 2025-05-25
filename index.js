0929196d25acfc60da05f6978402ee75c4a63443
Takudzwanashe <takudzwanashengaiso@gmail.com>
2025-05-25 10:39:49 +0200

    Vennis resources-BAck-end

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const db = require("./models");
 const cors = require("cors");
-
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
+
 // Routes
 const vennisStonesRoutes = require("./routes/VennisStones");
 app.use("/vennisstones", vennisStonesRoutes);
@@ -19,11 +19,19 @@ app.use("/usercontact", userContactRoutes);
 // Basic error handler
 app.use((err, req, res, next) => {
 	console.error(err.stack);
-	res.status(500).json({ error: "Something went wrong!" });
+	res
+		.status(err.status || 500)
+		.json({ error: err.message || "Something went wrong!" });
 });
-
-db.sequelize.sync().then(() => {
-	app.listen(process.env.PORT || 3001, () => {
-		console.log(`Server is running on port ${PORT}`);
+// Sync database and start server
+db.sequelize
+	.sync()
+	.then(() => {
+		app.listen(PORT, () => {
+			console.log(`Server is running on port ${PORT}`);
+		});
+	})
+	.catch((err) => {
+		console.error("Database connection error:", err);
+		process.exit(1); // Exit the application if the database connection fails
 	});
-});
